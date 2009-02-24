3052ccc03e158a8fc2c9a965fd3c6e51a5a4cae0
Rick Fletcher <fletch@pobox.com>
2009-02-24 15:03:18 -0800

    Removed the value container <div/>s from the markup in favor of inserting them at dom:loaded.

--- demo/demo.js
+++ demo/demo.js
@@ -1,13 +1,18 @@
 document.observe( 'dom:loaded', function() {
 
-  // hook up the "get value" buttons
-  $$( 'button' ).invoke( 'observe', 'click', function( e ) {
-    var wrapper_el = Event.element( e ).up( '.input-wrapper' );
-    var value_el = wrapper_el.down( '.value' );
+    // add the value containers
+    $$( 'button' ).each( function( button ) {
+        button.insert( { after: new Element( 'div', { 'class': 'value' } ) } );
+    } );
 
-    value_el.update(
-      TagBox.values( wrapper_el.down( '.tagbox' ) ).join( ', ' ).escapeHTML() || '(no value)'
-    );
-  } );
+    // hook up the "get value" buttons
+    $$( 'button' ).invoke( 'observe', 'click', function( e ) {
+        var wrapper_el = Event.element( e ).up( '.input-wrapper' );
+        var value_el = wrapper_el.down( '.value' );
+
+        value_el.update(
+            TagBox.values( wrapper_el.down( '.tagbox' ) ).join( ', ' ).escapeHTML() || '(no value)'
+        );
+    } );
 
 } );
