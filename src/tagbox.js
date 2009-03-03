01034736d6b68ddd348b46688ea1a8b2d7a480b7
Rick Fletcher <fletch@pobox.com>
2009-03-02 16:51:41 -0800

    Moved extra setup to lib/util.js to make tagbox.js a little clearer

--- src/tagbox.js
+++ src/tagbox.js
@@ -11,36 +11,7 @@
  * Portions retain his copyright.
  */
 
-// Add a few KEY_* constants to prototype's Event object
-Object.extend( Event, {
-    KEY_COMMA: 44,
-    KEY_SEMICOLON: 59,
-    KEY_SPACE: 32
-} );
-
-// Add methods to form input elements
-Object.extend( Form.Element.Methods, {
-    /**
-     * Form.Element.getCaretPosiiton( element ) -> Number
-     *   - element (Element): A text input element.
-     *
-     * Get a text input current caret position.
-     **/
-    getCaretPosition: function( element ) {
-        if( element.createTextRange ) {
-            var r = document.selection.createRange().duplicate();
-            r.moveEnd( 'character', element.value.length );
-            if( r.text === '' ) {
-                return element.value.length;
-            }
-            return element.value.lastIndexOf( r.text );
-        } else {
-            return element.selectionEnd ? element.selectionEnd : element.selectionStart;
-        }
-    }
-} );
-Element.addMethods();
-
+//= require "lib/util"
 //= require "lib/TagBox"
 //= require "lib/TagBox.Tag"
 //= require "lib/ElasticTextBox"
